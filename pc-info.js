f1640c5de3dab2695ed2da89436c2533a666eb71
Mustafa Kahraman <80291704+kahramanX@users.noreply.github.com>
2021-10-13 21:38:46 +0300

    some edit, added other pc info and text example

--- pc-info.js
+++ pc-info.js
@@ -1,23 +1,21 @@
 let fs = require('fs');
 let os = require('os');
 
-function getPcInfos() {
-    let ram = `Your free RAM: ${os.freemem()}`;
-    let totalRam = `Your total RAM: ${os.totalmem()}`;
-
-    let currentRam = os.totalmem() - os.freemem();
-    
-
-    return {
-        ram,
-        totalRam,
-        currentRam
-    };
-
+getPcInfosObj = {
+    ram:`Your free RAM: ${os.freemem()}`,
+    totalRam: `Your total RAM: ${os.totalmem()}`,
+    howManyCpu: `there is "${os.cpus().length}" cpu`,
+    arch: `architect : ${os.arch()}`,
+    platform: `platform : ${os.platform()}`,
+    upTime: `system upTime of seconds : ${os.uptime()}`,
+    version: `version : ${os.version()}`,
+    osType: `OS Type : ${os.type()}`
 }
 
-fs.writeFile("your-PC-infos.txt", JSON.stringify(getPcInfos(),null, 2), 'utf-8', function (err, data) {
+fs.writeFile("your-PC-infos.txt", JSON.stringify(getPcInfosObj, null, 2), 'utf-8', function (err, data) {
     if (err) {
         console.log(err);
     }
+    //success
+    console.log("success")
 });
